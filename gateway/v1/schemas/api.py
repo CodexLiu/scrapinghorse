5c0878fc103880e873b320fb476f346e754f1104
Mikhail <mikkho.ocampo@gmail.com>
2025-09-05 18:32:12 -0700

    Fix

--- gateway/v1/schemas/api.py
+++ gateway/v1/schemas/api.py
@@ -39,7 +39,7 @@ TextBlock = Union[ParagraphBlock, ListBlock]
 BARN_ANIMALS = ["cows", "sheep", "pigs", "chickens", "horses", "dogs", "cats", "rabbits", "fish", "birds"]
 
 class Metadata(BaseModel):
-    credits_used: str = Field(..., description="Credits used for the request")
+    credits_used: str = Field("0", description="Credits used for the request")
     version: str = Field("0.1.0", description="Version of the API")
     
     def set_credits_used(self):
